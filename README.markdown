093db7a2c7996174fd201e7004f9ace7fd174e3f
unknown <m.velimirov@gmail.com>
2021-06-15 09:05:29 +0200

    changed code base

--- README.markdown
+++ README.markdown
@@ -1,9 +1,11 @@
 ## Hacker News Chrome Extension  
 
-
-This is an extension for Google Chrome that displays the latest links from Y Combinator's [Hacker News](https://news.ycombinator.com). It also features a link for submitting the url of the current tab.
-
+This is an extension for Google Chrome that displays the latest links from Y Combinator's [Hacker News](https://news.ycombinator.com). 
+It was originally developed by Adam Albrecht.
+But looking at the source I felt the urge to clean it up to a large degree.
+Also, I remove search, which I wasn't using anyways.
+There is a saving function now, that can be issued by holding CTRL while clicking on a title.
 
 #### Installation:
 
-Click [here](https://chrome.google.com/webstore/detail/hacker-news/geancnifhbkbjijfkcjjdnfemppmcjmk) to install through the Chrome Web Store.
+Not available in the store yet
--- background.js
+++ background.js
@@ -1,11 +1,90 @@
+var maxFeedItems = 25;
+var retryMilliseconds = 120000;
 var firstRequest = true;
-function startRequest() {
-  UpdateIfReady(firstRequest);
-  firstRequest = false;
-  window.setTimeout(startRequest, 60000);
-}
-//If any options are not already set, they will be set to defaults here
-SetInitialOption("HN.RequestInterval", 1200000);
-SetInitialOption("HN.BackgroundTabs", false);
+var hashedLinks = {};
+var currentLinks = [];
+var savedLinks = localStorage["HN.savedLinks"] === undefined? [] : JSON.parse(localStorage["HN.savedLinks"]);
+var savedLinksHash = new Set();
+var newItems = 0;
 
-startRequest();
+var updateFeed = ()=> {
+  fetch('https://news.ycombinator.com/rss').then(async result => {
+    links = parseHNLinks(await result.text());
+    localStorage["HN.LastRefresh"] = (new Date()).getTime();
+  });
+}
+
+function parseHNLinks(rawXmlStr) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(rawXmlStr, "text/xml");
+  var entries = doc.getElementsByTagName('entry');
+  
+  if (entries.length == 0) {
+    entries = doc.getElementsByTagName('item');
+  }
+
+  var count = Math.min(entries.length, maxFeedItems);
+  
+  currentLinks = [];
+  hashedLinks ??= hashedLinks | {};
+
+  for (var i = 0; i < count; i++) {
+    item = entries.item(i);
+
+
+    var itemTitle = item.getElementsByTagName('title')[0]?.textContent;
+    var itemLink = item.getElementsByTagName('link')[0]?.textContent;
+    var commentsLink = item.getElementsByTagName('comments')[0]?.textContent;
+    var hnLink = {
+      link: itemLink || "", 
+      title: itemTitle || "Unknown Title", 
+      commentsLink: commentsLink || ""
+    };
+
+    if(hashedLinks[hnLink.title+hnLink.link]
+      || savedLinks[hnLink.title+hnLink.link]) continue; 
+    
+    currentLinks.push(hnLink);
+    newItems++;
+  }
+  
+  hashedLinks = {};
+
+  currentLinks.forEach(x=>{
+    hashedLinks[x.title+x.link] = true;
+  });
+
+  chrome.browserAction.setBadgeText({text: newItems > 0 ? newItems.toString() : "" });
+  
+  if(currentLinks.length > maxFeedItems) currentLinks = currentLinks.slice(currentLinks.length-maxFeedItems);
+}
+
+function openLink(e) {
+  e.preventDefault();
+  openUrl(this.href, (localStorage['HN.BackgroundTabs'] == 'false'));
+}
+
+function openLinkFront(e) {
+  e.preventDefault();
+  openUrl(this.href, true);
+}
+
+function openUrl(url, take_focus) {
+  if (url.indexOf("http:") != 0 && url.indexOf("https:") != 0) {
+    return;
+  }
+  chrome.tabs.create({url: url, selected: take_focus});
+}
+
+function saveLink(hnLink) {
+  savedLinks.push(hnLink);
+  savedLinksHash.add(hnLink.title+hnLink.link);
+  localStorage["HN.savedLinks"] = JSON.stringify(savedLinks);
+}
+
+chrome.alarms.create("refresh", {delayInMinutes: 15.0, periodInMinutes: 15.0} );
+chrome.alarms.onAlarm.addListener((alarm)=>{
+  updateFeed();
+});
+
+updateFeed();
